b3aff11eed45262dcd2dd7e69de28de9b5a6d6a0
Vinicius <vgaxavier@gmail.com>
2026-02-27 04:29:42 -0300

    bd

--- backend/src/utils/prisma.js
+++ backend/src/utils/prisma.js
@@ -1,5 +1,8 @@
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate'; // Se usar acelerador
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  // O Prisma 7 busca a conexão automaticamente do config ou adapter
+});
 
 export default prisma;
